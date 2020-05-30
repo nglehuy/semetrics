351623b93864f2e282c34f2429499686d7bf9a57
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-05-30 16:31:25 +0700

    Remove warnings

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from __future__ import absolute_import
 
+import os
+import logging
+import oct2py
 from pesq import pesq
 from scipy.io import wavfile
-from oct2py import octave
+
+logging.basicConfig(level=logging.ERROR)
+oc = oct2py.Oct2Py(logger=logging.getLogger())
 
 
 def pesq_mos(clean: str, enhanced: str):
@@ -14,7 +19,7 @@ def pesq_mos(clean: str, enhanced: str):
 
 
 def composite(clean: str, enhanced: str):
-    csig, cbak, covl, ssnr = octave.feval("composite", clean, enhanced, nout=4)
+    csig, cbak, covl, ssnr = oc.feval(os.path.join(os.path.dirname(__file__), "composite.m"), clean, enhanced, nout=4)
     pesq_score = pesq_mos(clean, enhanced)
     csig += 0.603 * pesq_score
     cbak += 0.478 * pesq_score
