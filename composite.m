2deffa83cc96793126df8e2fc9920de51ba275b0
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-06-01 12:21:09 +0700

    Change to LR

--- composite.m
+++ composite.m
@@ -454,37 +454,37 @@ end
 %---------------------------------------------
 function [acorr, refcoeff, lpparams] = lpcoeff(speech_frame, model_order)
 
-   % ----------------------------------------------------------
-   % (1) Compute Autocorrelation Lags
-   % ----------------------------------------------------------
+ % ----------------------------------------------------------
+ % (1) Compute Autocorrelation Lags
+ % ----------------------------------------------------------
 
-   winlength = max(size(speech_frame));
-   for k=1:model_order+1
-      R(k) = sum(speech_frame(1:winlength-k+1) ...
-		     .*speech_frame(k:winlength));
-   end
+ winlength = max(size(speech_frame));
+ for k=1:model_order+1
+    R(k) = sum(speech_frame(1:winlength-k+1) ...
+	     .*speech_frame(k:winlength));
+ end
 
-   % ----------------------------------------------------------
-   % (2) Levinson-Durbin
-   % ----------------------------------------------------------
+ % ----------------------------------------------------------
+ % (2) Levinson-Durbin
+ % ----------------------------------------------------------
 
-   a = ones(1,model_order);
-   E(1)=R(1);
-   for i=1:model_order
-      a_past(1:i-1) = a(1:i-1);
-      sum_term = sum(a_past(1:i-1).*R(i:-1:2));
-      rcoeff(i)=(R(i+1) - sum_term) / E(i);
-      a(i)=rcoeff(i);
-      a(1:i-1) = a_past(1:i-1) - rcoeff(i).*a_past(i-1:-1:1);
-      E(i+1)=(1-rcoeff(i)*rcoeff(i))*E(i);
-   end
+ a = ones(1,model_order);
+ E(1)=R(1);
+ for i=1:model_order
+    a_past(1:i-1) = a(1:i-1);
+    sum_term = sum(a_past(1:i-1).*R(i:-1:2));
+    rcoeff(i)=(R(i+1) - sum_term) / E(i);
+    a(i)=rcoeff(i);
+    a(1:i-1) = a_past(1:i-1) - rcoeff(i).*a_past(i-1:-1:1);
+    E(i+1)=(1-rcoeff(i)*rcoeff(i))*E(i);
+ end
 
-   acorr    = R;
-   refcoeff = rcoeff;
-   lpparams = [1 -a];
+ acorr    = R;
+ refcoeff = rcoeff;
+ lpparams = [1 -a];
 
-   
-   % ----------------------------------------------------------------------
+ 
+ % ----------------------------------------------------------------------
 
 function [overall_snr, segmental_snr] = snr(clean_speech, processed_speech,sample_rate)
 
@@ -557,6 +557,3 @@ for frame_count = 1: num_frames
    start = start + skiprate;
 
 end
-
-
-
